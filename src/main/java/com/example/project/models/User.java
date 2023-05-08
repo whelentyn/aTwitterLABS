39b50dc8458056a9f16867f7342c791eb2e5bf5f
whelentyn <valentin.bogulean@student.upt.ro>
2023-05-08 11:38:11 +0300

    deleted getters and setters for field within user model

--- src/main/java/com/example/project/models/User.java
+++ src/main/java/com/example/project/models/User.java
@@ -27,57 +27,6 @@ public class User {
 
     @OneToMany(mappedBy="from")
     private List<User> following;
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<User> getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(List<User> followers) {
-        this.followers = followers;
-    }
-
-    public List<User> getFollowing() {
-        return following;
-    }
-
-    public void setFollowing(List<User> following) {
-        this.following = following;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public long getId() {
-        return id;
-    }
 
     @Override
     public String toString() {
